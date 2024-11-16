ae61e0b3896c13f725f8f8e76d661dc98747a538
Dario Sanchez <dario.zyx@gmail.com>
2024-11-15 21:12:01 -0500

    Remove header (for now?)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { Separator } from "@/components/ui/separator";
 export default function Home() {
     return (
         <div className="w-full max-w-6xl p-8 flex flex-col">
-            <h1>Trade</h1>
             <div className="w-full grid grid-cols-[1fr_auto_2fr]">
                 <div>
                     <div className="p-4">You have $245,073 and 512 GPUs</div>
