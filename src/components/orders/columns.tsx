87c0e06a71439071482eda567bb9300ecb7ea3c1
Dario Sanchez <dario.zyx@gmail.com>
2024-11-16 18:08:45 -0500

    Re-add cancel btn

--- src/components/orders/columns.tsx
+++ src/components/orders/columns.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
 
 export type TOrder = {
@@ -55,6 +56,20 @@ const ordersColumns: ColumnDef<TOrder>[] = [
             const v = row.getValue("status") as string;
             const color = v === "Filled" ? "text-green-600" : v === "Pending" ? "text-yellow-600" : "text-red-600";
             return <div className={color}>{v}</div>;
+        },
+    },
+    {
+        id: "actions",
+        cell: ({ row: { original } }) => {
+            return original.status === "Pending" ? (
+                <Button
+                    variant="outline"
+                    size="sm"
+                    className="p-2 h-6 bg-zinc-100 hover:bg-red-500 hover:border-red-600 hover:text-white"
+                >
+                    Cancel
+                </Button>
+            ) : null;
         },
     },
 ];
