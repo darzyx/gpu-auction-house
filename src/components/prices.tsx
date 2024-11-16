a10876e562a5eed38977b3e9c02fece9b9b9ed83
Dario Sanchez <dario.zyx@gmail.com>
2024-11-16 13:14:33 -0500

    Add last average and availability to chart

--- src/components/prices.tsx
+++ src/components/prices.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
-import { TrendingUp } from "lucide-react";
+import { ArrowRight, TrendingUp } from "lucide-react";
 import { CartesianGrid, LabelList, Line, LineChart, XAxis, YAxis } from "recharts";
+import { Separator } from "./ui/separator";
 
 const chartData = [
-    { day: "Sunday", high: 1.68, average: 1.2, low: 0.75 },
-    { day: "Monday", high: 1.41, average: 0.84, low: 0.4 },
-    { day: "Tuesday", high: 1.85, average: 1.2, low: 0.6 },
-    { day: "Wednesday", high: 1.75, average: 1.0, low: 0.3 },
-    { day: "Thursday", high: 1.85, average: 1.4, low: 0.8 },
-    { day: "Friday", high: 1.45, average: 1, low: 0.39 },
-    { day: "Saturday", high: 1.72, average: 1.15, low: 0.65 },
+    { day: "Sunday", high: 1.8, average: 1.32, low: 0.87 },
+    { day: "Monday", high: 1.43, average: 0.86, low: 0.42 },
+    { day: "Tuesday", high: 2.07, average: 1.42, low: 0.82 },
+    { day: "Wednesday", high: 1.97, average: 1.22, low: 0.52 },
+    { day: "Thursday", high: 2.07, average: 1.62, low: 1.02 },
+    { day: "Friday", high: 1.67, average: 1.22, low: 0.61 },
+    { day: "Saturday", high: 1.94, average: 1.37, low: 0.87 },
 ];
 
 const chartConfig = {
@@ -22,14 +23,30 @@ const chartConfig = {
 
 export default function Prices() {
     return (
-        <div>
-            <h2 className="text-lg font-georgia">Average Prices</h2>
+        <div className="flex flex-col justify-between gap-4">
+            <div className="grid grid-cols-[auto_1fr_auto_auto] items-end gap-8">
+                <div className="flex flex-col">
+                    <h2 className="text-lg font-georgia">Average Prices</h2>
+                    <div className="flex items-center gap-1 text-muted-foreground text-sm leading-none">
+                        Dec 15, 2024 - Dec 21, 2024
+                    </div>
+                </div>
+                <div />
+                <div className="flex flex-col text-xs">
+                    <div className="text-muted-foreground leading-none uppercase">Last Average</div>
+                    <h2 className="font-berkeley-mono">$1.37/GPU/hour</h2>
+                </div>
+                <div className="flex flex-col text-xs">
+                    <div className="text-muted-foreground leading-none uppercase">Availability</div>
+                    <h2 className="font-berkeley-mono">3258 GPUs</h2>
+                </div>
+            </div>
             <div>
                 <ChartContainer config={chartConfig} className="font-berkeley-mono">
                     <LineChart
                         accessibilityLayer
                         data={chartData}
-                        margin={{ left: -10, right: 20, top: 20, bottom: 20 }}
+                        margin={{ left: -10, right: 20, top: 0, bottom: 0 }}
                         height={300}
                     >
                         <CartesianGrid vertical={false} />
@@ -99,9 +116,11 @@ export default function Prices() {
                 </ChartContainer>
             </div>
             <div className="flex justify-between items-center text-muted-foreground font-medium text-sm leading-none">
-                <div>Dec 12 - Dec 18, 2024</div>
                 <div className="flex items-center gap-2">
                     Average up $0.15 from yesterday <TrendingUp className="h-4 w-4" />
+                </div>
+                <div className="flex items-center gap-2">
+                    Learn how the market works <ArrowRight className="h-4 w-4" />
                 </div>
             </div>
         </div>
