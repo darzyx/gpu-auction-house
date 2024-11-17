243a43fcea8e741d2c070dfe0b97386516ff6b6e
Dario Sanchez <dario.zyx@gmail.com>
2024-11-17 17:28:14 -0500

    Delete limit info box

--- src/components/trade/limit-info-box.tsx
+++
@@ -1,7 +0,0 @@
-export default function LimitInfoBox() {
-    return (
-        <div className="bg-gray-100 p-4 rounded-md mb-4 text-sm text-gray-600 text-center">
-            Your order will be filled when a matching offer becomes available.
-        </div>
-    );
-}
