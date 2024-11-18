fd86d0e631452270638262592d5ea2c9934aa18d
Dario Sanchez <dario.zyx@gmail.com>
2024-11-18 10:40:58 -0500

    Add link color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Link from "next/link";
 export default async function Home() {
     return (
         <main className="w-full h-full max-w-7xl p-0 lg:p-8">
-            Hi, this is an unfinished home page. You&apos;re looking for the <Link href="/trade">/trade</Link> page.
+            Hi, this is an unfinished home page. You&apos;re looking for the{" "}
+            <Link href="/trade" className="text-sky-600">
+                /trade
+            </Link>{" "}
+            page.
         </main>
     );
 }
