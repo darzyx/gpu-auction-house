241632fe1fd9b6bc8a63a6c00a648e8cede08bf9
Dario Sanchez <dario.zyx@gmail.com>
2024-11-18 00:44:36 -0500

    Remove drizzle for now

--- src/db/index.ts
+++
@@ -1,6 +0,0 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
-import { orders } from "./schema";
-
-export const db = drizzle(sql);
-export { orders };
