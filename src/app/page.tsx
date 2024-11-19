8f575da6131ef6df523c9f14c1c68f8a72a3b562
Dario Sanchez <dario.zyx@gmail.com>
2024-11-19 09:59:33 -0500

    Simplify home page copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default async function Home() {
     return (
         <main className="w-full h-full max-w-7xl p-8 flex justify-center">
-            Hi, this is an unfinished home page. You&apos;re looking for the{" "}
+            You&apos;re looking for the{" "}
             <Link href="/trade" className="text-sky-600">
                 /trade
             </Link>{" "}
