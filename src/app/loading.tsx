1cb765f99a16c860b94139801e0d395d9750ba3a
Dario Sanchez <dario.zyx@gmail.com>
2024-11-19 16:07:27 -0500

    Give the loading page a nicer font

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
         <div className="w-full h-full flex flex-col justify-center items-center">
-            <p className="leading-none">Loading...</p>
+            <p className="leading-none font-berkeley-mono">Loading...</p>
         </div>
     );
 }
