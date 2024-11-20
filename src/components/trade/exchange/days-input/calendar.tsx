9ab7058c0d958e08a9f03ff5ebf47dc16ff68cfc
Dario Sanchez <dario.zyx@gmail.com>
2024-11-20 18:37:02 -0500

    Improve calendar UX by using a desktop dialog and mobile drawer

--- src/components/trade/exchange/days-input/calendar.tsx
+++ src/components/trade/exchange/days-input/calendar.tsx
@@ -35,13 +35,13 @@ function Calendar({
                 nav: "space-x-1 flex items-center",
                 nav_button: cn(
                     buttonVariants({ variant: "outline" }),
-                    "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
+                    "h-8 w-8 bg-transparent p-0 opacity-50 hover:opacity-100"
                 ),
                 nav_button_previous: "absolute left-1",
                 nav_button_next: "absolute right-1",
                 table: "w-full border-collapse space-y-1",
                 head_row: "flex",
-                head_cell: "text-muted-foreground rounded-md w-11 lg:w-14 font-normal text-[0.8rem]",
+                head_cell: "text-muted-foreground rounded-md w-11 sm:w-12 lg:w-16 font-normal text-[0.8rem]",
                 row: "flex w-full mt-2",
                 cell: cn(
                     "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
@@ -51,7 +51,7 @@ function Calendar({
                 ),
                 day: cn(
                     buttonVariants({ variant: "ghost" }),
-                    "w-11 h-11 lg:w-14 lg:h-14 font-normal aria-selected:opacity-100 flex flex-col justify-center items-center"
+                    "w-11 h-11 sm:w-12 sm:h-12 lg:w-16 lg:h-16 font-normal aria-selected:opacity-100 flex flex-col justify-center items-center"
                 ),
                 day_range_start: "day-range-start",
                 day_range_end: "day-range-end",
@@ -80,11 +80,11 @@ function Calendar({
                     const isRangeEnd = props.activeModifiers.range_end;
                     return (
                         <>
-                            <span className="text-xs lg:text-sm">{props.date.getDate()}</span>
+                            <span className="text-xs sm:text-sm lg:text-base">{props.date.getDate()}</span>
                             {renderPrice && (
                                 <span
                                     className={cn(
-                                        "text-[0.6rem] lg:text-xs leading-none -mt-1.5 font-berkeley-mono",
+                                        "text-[0.6rem] lg:text-sm leading-none -mt-1.5 font-berkeley-mono",
                                         makePriceVisible
                                             ? isRangeStart
                                                 ? "text-white"
--- src/components/trade/exchange/days-input/index.tsx
+++ src/components/trade/exchange/days-input/index.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { useMediaQuery } from "@/hooks/use-media-query";
 import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { DateRange } from "react-day-picker";
 
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Drawer, DrawerClose, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import { Label } from "@/components/ui/label";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { useMediaQuery } from "@/hooks/use-media-query";
 import { cn } from "@/lib/utils";
-import { PopoverClose } from "@radix-ui/react-popover";
-import { CalendarIcon } from "lucide-react";
 import { OrderFormData, OrderType } from "../types";
 import {
     formatCurrency,
@@ -38,6 +38,7 @@ export default function DaysInput({
     isBuy,
 }: DaysInputProps) {
     const isDesktop = useMediaQuery("(min-width: 768px)");
+    const [open, setOpen] = useState(false);
     const [dayAmounts, setDayAmounts] = useState<Record<string, string>>({});
 
     const handleSelect = (range: DateRange | undefined) => {
@@ -78,85 +79,107 @@ export default function DaysInput({
 
     const numSelectedDays = days ? getNumerOfDaysSelected(days) : 0;
 
+    const TriggerButton = (
+        <Button
+            id="date"
+            variant="outline"
+            className={cn("w-full justify-start text-left font-normal", !days && "text-muted-foreground")}
+        >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {days?.from ? (
+                days.to ? (
+                    <>
+                        {format(days.from, "M/d/yy")} - {format(days.to, "M/d/yy")}
+                    </>
+                ) : (
+                    format(days.from, "M/d/yy")
+                )
+            ) : (
+                <span>Select dates</span>
+            )}
+        </Button>
+    );
+
+    const CalendarContent = () => (
+        <div>
+            <Calendar
+                initialFocus
+                mode="range"
+                defaultMonth={new Date()}
+                selected={days}
+                onSelect={handleSelect}
+                numberOfMonths={isDesktop ? 2 : 1}
+                fromDate={new Date()}
+                dayAmounts={dayAmounts}
+                hasPickedQuantity={!!quantity}
+                isMarketOrder={orderType === "market"}
+            />
+            <div className="flex justify-end items-center gap-1 mt-2">
+                <div className="flex items-center h-10 p-2 text-sm md:text-base">
+                    {orderType === "limit" ? (
+                        ""
+                    ) : !quantity ? (
+                        <span className="flex flex-col justify-center items-end text-xs md:text-sm text-muted-foreground">
+                            <span>Add quantity (GPUs)</span>
+                            <span>to see prices</span>
+                        </span>
+                    ) : days?.from && days?.to ? (
+                        <span className="flex flex-col justify-center items-end">
+                            <span className="flex justify-start items-center gap-1">
+                                <span className="text-xs text-muted-foreground">{isBuy ? "from" : "up to"}</span>
+                                <span className="font-berkeley-mono">{formatCurrency(getSelectedRangePrice())}</span>
+                            </span>
+                            <span className="text-xs text-muted-foreground">per GPU per day</span>
+                        </span>
+                    ) : (
+                        <span className="flex flex-col justify-center items-end text-xs md:text-sm text-muted-foreground">
+                            <span>Select start and end</span>
+                            <span>dates to see prices</span>
+                        </span>
+                    )}
+                </div>
+                {isDesktop ? (
+                    <DialogClose asChild>
+                        <Button className="w-20 h-10">Done</Button>
+                    </DialogClose>
+                ) : (
+                    <DrawerClose asChild>
+                        <Button className="w-20 h-10">Done</Button>
+                    </DrawerClose>
+                )}
+            </div>
+        </div>
+    );
+
     return (
         <div className="space-y-1">
             <Label htmlFor="days" className="text-xs">
                 DATE RANGE
             </Label>
             <div className={cn("relative w-full grid gap-2", className)}>
-                <Popover>
-                    <PopoverTrigger asChild>
-                        <Button
-                            id="date"
-                            variant={"outline"}
-                            className={cn(
-                                "w-full justify-start text-left font-normal",
-                                !days && "text-muted-foreground"
-                            )}
-                        >
-                            <CalendarIcon />
-                            {days?.from ? (
-                                days.to ? (
-                                    <>
-                                        {format(days.from, "M/d/yy")} - {format(days.to, "M/d/yy")}
-                                    </>
-                                ) : (
-                                    format(days.from, "M/d/yy")
-                                )
-                            ) : (
-                                <span>Select dates</span>
-                            )}
-                        </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
-                        <Calendar
-                            initialFocus
-                            mode="range"
-                            defaultMonth={new Date()}
-                            selected={days}
-                            onSelect={handleSelect}
-                            numberOfMonths={isDesktop ? 2 : 1}
-                            fromDate={new Date()}
-                            dayAmounts={dayAmounts}
-                            hasPickedQuantity={!!quantity}
-                            isMarketOrder={orderType === "market"}
-                        />
-                        <div className="flex justify-end items-center gap-1">
-                            <div className="flex items-center h-10 p-2 text-sm md:text-base -mt-2 mb-3">
-                                {orderType === "limit" ? (
-                                    ""
-                                ) : !quantity ? (
-                                    <span className="flex flex-col justify-center items-end text-xs md:text-sm text-muted-foreground">
-                                        <span>Add quantity (GPUs)</span>
-                                        <span>to see prices</span>
-                                    </span>
-                                ) : days?.from && days?.to ? (
-                                    <span className="flex flex-col justify-center items-end">
-                                        <span className="flex justify-start items-center gap-1">
-                                            <span className="text-xs text-muted-foreground">
-                                                {isBuy ? "from" : "up to"}
-                                            </span>
-                                            <span className="font-berkeley-mono">
-                                                {formatCurrency(getSelectedRangePrice())}
-                                            </span>
-                                        </span>
-                                        <span className="text-xs text-muted-foreground">per GPU per day</span>
-                                    </span>
-                                ) : (
-                                    <span className="flex flex-col justify-center items-end text-xs md:text-sm text-muted-foreground">
-                                        <span>Select start and end</span>
-                                        <span>dates to see prices</span>
-                                    </span>
-                                )}
+                {isDesktop ? (
+                    <Dialog open={open} onOpenChange={setOpen}>
+                        <DialogTrigger asChild>{TriggerButton}</DialogTrigger>
+                        <DialogContent className="w-full max-w-fit">
+                            <DialogHeader>
+                                <DialogTitle>Select Date Range</DialogTitle>
+                            </DialogHeader>
+                            <div className="flex flex-col justify-center items-center">
+                                <CalendarContent />
                             </div>
-                            <PopoverClose>
-                                <div className="flex justify-center items-center h-10 border rounded-md p-2 text-sm -mt-2 mr-3 mb-3 min-w-28 bg-foreground text-white hover:opacity-90">
-                                    Done
-                                </div>
-                            </PopoverClose>
-                        </div>
-                    </PopoverContent>
-                </Popover>
+                        </DialogContent>
+                    </Dialog>
+                ) : (
+                    <Drawer open={open} onOpenChange={setOpen}>
+                        <DrawerTrigger asChild>{TriggerButton}</DrawerTrigger>
+                        <DrawerContent className="flex flex-col justify-center items-center pb-10">
+                            <DrawerHeader className="hidden">
+                                <DrawerTitle>Select Date Range</DrawerTitle>
+                            </DrawerHeader>
+                            <CalendarContent />
+                        </DrawerContent>
+                    </Drawer>
+                )}
                 <div className="absolute -bottom-5 flex flex-col justify-center items-end">
                     <span className="text-muted-foreground text-xs">
                         You have {numSelectedDays} {numSelectedDays === 1 ? "day" : "days"} selected
