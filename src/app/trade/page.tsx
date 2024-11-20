71c73ae9aabf734e097387acde960c08f2abb3ed
Dario Sanchez <dario.zyx@gmail.com>
2024-11-20 09:43:34 -0500

    Try new baseUrl to see if this works both in prod and locally

--- src/app/trade/page.tsx
+++ src/app/trade/page.tsx
@@ -4,7 +4,8 @@ import { TOrder } from "@/types";
 import TradeParent from "./trade-parent";
 
 async function getOrders() {
-    const res = await fetch(process.env.VERCEL_URL + "/api/orders", { method: "GET" });
+    const baseUrl = process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_API_URL : "";
+    const res = await fetch(`${baseUrl}/api/orders`, { method: "GET" });
     if (!res.ok) throw new Error("Failed to fetch orders");
     return res.json();
 }
