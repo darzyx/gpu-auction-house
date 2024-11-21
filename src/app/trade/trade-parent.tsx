33b3fa82572d408cc709e7b2612e2c0cd2844454
Dario Sanchez <dario.zyx@gmail.com>
2024-11-20 19:17:36 -0500

    Improve padding for  major sections

--- src/app/trade/trade-parent.tsx
+++ src/app/trade/trade-parent.tsx
@@ -22,7 +22,7 @@ export default function TradeParent({ initialOrders }: { initialOrders: TOrderFr
 
     return (
         <main className="w-full max-w-7xl h-full p-0 lg:p-8 grid grid-rows-[auto_auto_1fr] lg:grid-rows-1 lg:grid-cols-[auto_auto_1fr]">
-            <div className="lg:h-full p-4">
+            <div className="lg:h-full p-4 sm:p-6 lg:p-4">
                 <Navigation />
             </div>
             <Separator orientation="vertical" className="hidden lg:block" />
@@ -30,23 +30,23 @@ export default function TradeParent({ initialOrders }: { initialOrders: TOrderFr
             <div className="grid grid-rows-[auto_auto_1fr] h-full">
                 <div className="w-full grid grid-cols-1 md:grid-cols-[375px_auto_1fr] lg:grid-cols-[400px_auto_1fr]">
                     <div>
-                        <div className="p-4">
+                        <div className="p-4 sm:p-6 lg:p-4">
                             <Portfolio />
                         </div>
                         <Separator />
-                        <div className="p-4">
+                        <div className="p-4 sm:p-6 lg:p-4">
                             <Exchange onOrderSubmitted={handleNewOrder} />
                         </div>
                     </div>
                     <Separator className="md:hidden" />
                     <Separator orientation="vertical" className="hidden md:block" />
-                    <div className="p-4">
+                    <div className="p-4 sm:p-6 lg:p-4">
                         <Prices />
                     </div>
                 </div>
                 <Separator />
                 <div className="relative">
-                    <div className="pb-10 md:pb-4 p-4 w-full absolute md:inset-0 md:overflow-auto">
+                    <div className="pb-10 md:pb-4 p-4 sm:p-6 lg:p-4 w-full absolute md:inset-0 md:overflow-auto">
                         <Orders data={orders} columns={ordersColumns} />
                     </div>
                 </div>
