77dfcb923b0dde2540ce8eca7c173580a304588f
Dario Sanchez <dario.zyx@gmail.com>
2024-11-21 11:07:45 -0500

    Add padding to loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-        <div className="w-full h-full flex flex-col justify-center items-center">
+        <div className="w-full h-full p-8 flex flex-col justify-center items-center">
             <p className="leading-none font-berkeley-mono">Loading...</p>
         </div>
     );
