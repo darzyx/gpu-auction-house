337a027f703a5b78d0d7680ea538f51928c6796f
Dario Sanchez <dario.zyx@gmail.com>
2024-11-24 15:07:58 -0500

    Add donut chart to metrics page

---
+++ src/app/metrics/availability-chart.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { Label, Pie, PieChart } from "recharts";
+
+import { AVAILABLE_GPUS, TOTAL_GPUS } from "@/components/trade/exchange/utils";
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartData = [
+    {
+        category: "high",
+        amount: Math.round(AVAILABLE_GPUS * 0.3),
+        fill: "var(--color-high)",
+    },
+    {
+        category: "average",
+        amount: Math.round(AVAILABLE_GPUS * 0.5),
+        fill: "var(--color-average)",
+    },
+    {
+        category: "low",
+        amount: Math.round(AVAILABLE_GPUS * 0.2),
+        fill: "var(--color-low)",
+    },
+    {
+        category: "unavailable",
+        amount: TOTAL_GPUS - AVAILABLE_GPUS,
+        fill: "var(--color-other)",
+    },
+];
+
+const chartConfig = {
+    high: {
+        label: "High",
+        color: "hsl(var(--chart-1))",
+    },
+    average: {
+        label: "Average",
+        color: "hsl(var(--chart-2))",
+    },
+    low: {
+        label: "Low",
+        color: "hsl(var(--chart-3))",
+    },
+    unavailable: {
+        label: "Unavailable",
+        color: "hsl(var(--chart-4))",
+    },
+} satisfies ChartConfig;
+
+export default function AvailabilityChart() {
+    return (
+        <ChartContainer
+            config={chartConfig}
+            className="w-full aspect-square h-[250px]"
+        >
+            <PieChart>
+                <ChartTooltip
+                    cursor={false}
+                    content={<ChartTooltipContent hideLabel />}
+                />
+                <Pie
+                    data={chartData}
+                    dataKey="amount"
+                    nameKey="category"
+                    innerRadius={60}
+                    strokeWidth={5}
+                >
+                    <Label
+                        content={({ viewBox }) => {
+                            if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                return (
+                                    <text
+                                        x={viewBox.cx}
+                                        y={viewBox.cy}
+                                        textAnchor="middle"
+                                        dominantBaseline="middle"
+                                    >
+                                        <tspan
+                                            x={viewBox.cx}
+                                            y={viewBox.cy}
+                                            className="fill-foreground text-3xl font-bold"
+                                        >
+                                            {TOTAL_GPUS}
+                                        </tspan>
+                                        <tspan
+                                            x={viewBox.cx}
+                                            y={(viewBox.cy || 0) + 24}
+                                            className="fill-muted-foreground"
+                                        >
+                                            GPUs
+                                        </tspan>
+                                    </text>
+                                );
+                            }
+                        }}
+                    />
+                </Pie>
+            </PieChart>
+        </ChartContainer>
+    );
+}
--- src/app/metrics/index.tsx
+++ src/app/metrics/index.tsx
@@ -1,9 +1,20 @@
 import PricesChart from "@/components/trade/prices-chart";
+import { Separator } from "@/components/ui/separator";
+import AvailabilityChart from "./availability-chart";
 
 export default function Prices() {
     return (
-        <main className="w-full h-full p-4 sm:p-6 lg:p-4">
-            <PricesChart />
+        <main className="w-full h-full">
+            <div className="grid grid-cols-[2fr_auto_auto]">
+                <div className="p-4 sm:p-6 lg:p-4">
+                    <PricesChart />
+                </div>
+                <Separator orientation="vertical" />
+                <div className="w-full">
+                    <AvailabilityChart />
+                    <Separator />
+                </div>
+            </div>
         </main>
     );
 }
