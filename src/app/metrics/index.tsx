19d933bec02125500dbf7a3ef89f00853307ad6d
Dario Sanchez <dario.zyx@gmail.com>
2024-11-24 15:15:56 -0500

    Make metrics page a little more responsive

--- src/app/metrics/index.tsx
+++ src/app/metrics/index.tsx
@@ -1,15 +1,22 @@
+"use client";
+
 import PricesChart from "@/components/trade/prices-chart";
 import { Separator } from "@/components/ui/separator";
+import { useMediaQuery } from "@/hooks/use-media-query";
 import AvailabilityChart from "./availability-chart";
 
 export default function Prices() {
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+
     return (
         <main className="w-full h-full">
-            <div className="grid grid-cols-[2fr_auto_auto]">
+            <div className="md:grid md:grid-cols-[2fr_auto_auto]">
                 <div className="p-4 sm:p-6 lg:p-4">
                     <PricesChart />
                 </div>
-                <Separator orientation="vertical" />
+                <Separator
+                    orientation={isDesktop ? "vertical" : "horizontal"}
+                />
                 <div className="w-full">
                     <AvailabilityChart />
                     <Separator />
