f912d6c0214ad61d0c7803905bc86baa2755f3af
Dario Sanchez <dario.zyx@gmail.com>
2024-11-24 14:22:29 -0500

    Add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <main className="w-full h-full max-w-7xl p-8 flex flex-col justify-center items-center gap-2">
+            <h1 className="text-xl font-georgia leading-none">404 Not Found</h1>
+            <p>
+                You&apos;re looking for the{" "}
+                <Link href="/trade" className="text-sky-600">
+                    /trade
+                </Link>{" "}
+                page.
+            </p>
+        </main>
+    );
+}
