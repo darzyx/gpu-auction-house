6d20cff752a86b3ff929e7dc6cdaecab7b9edad1
Dario Sanchez <dario.zyx@gmail.com>
2024-11-23 20:21:15 -0500

    Improve no orders yet placeholder for table

--- src/components/trade/orders/index.tsx
+++ src/components/trade/orders/index.tsx
@@ -33,6 +33,8 @@ export default function Orders({
         meta: { onOrderCanceled },
     });
 
+    const hasOrders = !!table.getRowModel().rows?.length;
+
     return (
         <div className="space-y-1">
             <h2 className="text-lg font-georgia leading-none">Orders</h2>
@@ -56,39 +58,40 @@ export default function Orders({
                     ))}
                 </TableHeader>
                 <TableBody>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
-                                {row.getVisibleCells().map((cell, idx) => {
-                                    let className = "h-12";
-                                    if (idx === 0) className += " pl-0";
-                                    if (idx === row.getVisibleCells().length - 1) className += " pr-0";
-                                    return (
-                                        <TableCell key={cell.id} className={className}>
-                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                        </TableCell>
-                                    );
-                                })}
-                            </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell colSpan={columns.length} className="h-24 text-center">
-                                No results.
-                            </TableCell>
-                        </TableRow>
-                    )}
+                    {hasOrders
+                        ? table.getRowModel().rows.map((row) => (
+                              <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                                  {row.getVisibleCells().map((cell, idx) => {
+                                      let className = "h-12";
+                                      if (idx === 0) className += " pl-0";
+                                      if (idx === row.getVisibleCells().length - 1) className += " pr-0";
+                                      return (
+                                          <TableCell key={cell.id} className={className}>
+                                              {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                          </TableCell>
+                                      );
+                                  })}
+                              </TableRow>
+                          ))
+                        : null}
                 </TableBody>
             </Table>
-            <div className="w-full flex justify-center items-center">
-                <Link
-                    href="/trade"
-                    className="group flex items-center gap-1 text-sm text-muted-foreground font-medium hover:underline underline-offset-2 cursor-pointer"
-                >
-                    See all orders
-                    <ArrowRight className="h-4 w-4 group-hover:translate-x-1 transition-transform" />
-                </Link>
-            </div>
+            {hasOrders && (
+                <div className="w-full flex justify-center items-center">
+                    <Link
+                        href="/trade"
+                        className="group flex items-center gap-1 text-sm text-muted-foreground font-medium hover:underline underline-offset-2 cursor-pointer"
+                    >
+                        See all orders
+                        <ArrowRight className="h-4 w-4 group-hover:translate-x-1 transition-transform" />
+                    </Link>
+                </div>
+            )}
+            {!hasOrders && (
+                <div className="w-full flex justify-center items-center h-20 lg:h-40">
+                    <p className="text-sm text-muted-foreground">You have&apos;t placed any orders yet.</p>
+                </div>
+            )}
         </div>
     );
 }
