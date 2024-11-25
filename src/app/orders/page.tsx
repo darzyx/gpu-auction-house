647000e075b2364f4891fd0b4831b738624debcb
Dario Sanchez <dario.zyx@gmail.com>
2024-11-25 00:02:11 -0500

    Make orders table span full height on orders page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -5,7 +5,7 @@ import OrdersSection from "@/components/trade/orders-section";
 export default function Page() {
     return (
         <main className="w-full h-full p-4 sm:p-6 lg:p-4">
-            <div className="relative h-[500px]">
+            <div className="relative h-full">
                 <div className="absolute inset-0 overflow-x-auto">
                     <OrdersSection />
                 </div>
