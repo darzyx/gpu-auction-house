78691144d0a8880f0641eabfed04c32108b9a8f1
Dario Sanchez <dario.zyx@gmail.com>
2024-11-25 00:07:03 -0500

    Fix bug where the order section was not showing up on mobile

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     return (
         <main className="w-full h-full p-4 sm:p-6 lg:p-4">
             <div className="relative w-full h-full">
-                <div className="absolute inset-0 overflow-x-auto">
+                <div className="absolute top-0 left-0 right-0 overflow-x-auto">
                     <OrdersSection />
                 </div>
             </div>
